4447ed926e9208931625f000a535d3f292b9b262
njtierney <nicholas.tierney@gmail.com>
2025-07-30 18:55:57 +1000

    import inst/dev - see #9. Unfortunately encountering some errors, I'll resolve separately.

---
+++ inst/dev/check_all_surveys.r
@@ -0,0 +1,23 @@
+library(contactsurveys)
+library(purrr)
+library(here)
+
+## load list of survey files
+survey_files <- readRDS(here("surveys", "survey_files.rds"))
+
+## define safe checking function
+safe_as_contact_survey <- safely(\(files) {
+  as_contact_survey(load_survey(files))
+})
+
+## check all surveys
+checks <- map(survey_files, safe_as_contact_survey)
+
+errors <- map(checks, "error")
+no_error <- map_vec(errors, is.null)
+error_messages <- map(errors[!no_error], "message")
+
+cat("Errors:\n\n")
+error_messages
+cat("\n\nWarnings:\n\n")
+warnings()
---
+++ inst/dev/dl_all_surveys.r
@@ -0,0 +1,23 @@
+library(contactsurveys)
+library(here)
+library(tictoc)
+
+## list all surveys
+ls <- list_surveys()
+
+dir.create(here("surveys"), showWarnings = FALSE)
+## download all surveys using the `url` column in the survey list and
+## save them in the `surveys` folder (which is created if it does not exist)
+tic()
+survey_files <- purrr::map(ls$url, function(x) {
+  download_survey(
+    survey = x,
+    dir = "surveys",
+    sleep = 10
+  )
+})
+toc()
+## name list elements according to url
+names(survey_files) <- paste0(ls$title, " (", ls$url, ")")
+## save list of survey files
+saveRDS(survey_files, here("surveys", "survey_files.rds"))
