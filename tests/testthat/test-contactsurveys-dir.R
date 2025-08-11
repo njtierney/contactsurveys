55f4c6d69b5d193c1b8913b2f3a61aeb2eaa2124
njtierney <nicholas.tierney@gmail.com>
2025-08-11 11:35:20 +1000

    Add an extra test to contactsurveys_dir() to check what happens if CONTACTSURVEYS_HOME is unset

--- tests/testthat/test-contactsurveys-dir.R
+++ tests/testthat/test-contactsurveys-dir.R
@@ -2,3 +2,13 @@ test_that("contactsurveys_dir()", {
   x <- contactsurveys_dir()
   expect_identical(x, Sys.getenv("CONTACTSURVEYS_HOME"))
 })
+
+test_that("contactsurveys_dir() falls back to R_user_dir when env unset", {
+  withr::local_envvar(CONTACTSURVEYS_HOME = NA_character_)
+  p <- contactsurveys_dir()
+  expect_true(nzchar(p))
+  expect_true(
+    dir.exists(p),
+    info = "contactsurveys_dir() should ensure the directory exists"
+  )
+})
