777eb9890b393502fa48887097410c58fc661ed6
njtierney <nicholas.tierney@gmail.com>
2025-08-11 18:46:05 +1000

    identifying where the issues with downloads are

--- inst/dev/dl_all_surveys.r
+++ inst/dev/dl_all_surveys.r
@@ -1,6 +1,7 @@
 library(contactsurveys)
 library(here)
 library(tictoc)
+library(purrr)
 
 ## list all surveys
 ls <- list_surveys()
@@ -8,15 +9,26 @@ ls <- list_surveys()
 dir.create(here("surveys"), showWarnings = FALSE)
 ## download all surveys using the `url` column in the survey list and
 ## save them in the `surveys` folder (which is created if it does not exist)
+# use `possibly` to ensure this continues running
+
+possibly_download_survey <- possibly(.f = download_survey, otherwise = NULL)
+
 tic()
-survey_files <- purrr::map(ls$url, function(x) {
-  Sys.sleep(10)
-  download_survey(
+survey_files <- map(ls$url, function(x) {
+  Sys.sleep(5)
+  survey_downloads <- possibly_download_survey(
     survey = x,
     dir = "surveys"
   )
 })
+
+
 toc()
+# identify issue with download
+which_did_not_download <- which(map(survey_files, is.null) |> flatten_lgl())
+
+browseURL(ls$url[which_did_not_download])
+https://zenodo.org/records/3878754
 ## name list elements according to url
 names(survey_files) <- paste0(ls$title, " (", ls$url, ")")
 ## save list of survey files
