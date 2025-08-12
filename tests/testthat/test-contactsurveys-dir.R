6c0a25375b21312faff6008975fb2531ba71c94e
njtierney <nicholas.tierney@gmail.com>
2025-08-12 16:47:01 +1000

    test that it works when unsetting envvar

--- tests/testthat/test-contactsurveys-dir.R
+++ tests/testthat/test-contactsurveys-dir.R
@@ -3,19 +3,22 @@ test_that("contactsurveys_dir()", {
   expect_identical(x, Sys.getenv("CONTACTSURVEYS_HOME"))
 })
 
-test_that("contactsurveys_dir() falls back to R_user_dir when env unset", {
+test_that("contactsurveys_dir() falls back to R_user_dir when env set to NA", {
   withr::local_envvar(CONTACTSURVEYS_HOME = NA_character_)
   p <- contactsurveys_dir()
   expect_true(nzchar(p))
-  expect_true(
-    file.exists(p),
-    info = "contactsurveys_dir() provides a real path"
+  expect_identical(
+    p,
+    tools::R_user_dir("contactsurveys")
   )
 })
 
-test_that("empty CONTACTSURVEYS_HOME falls back and creates dir", {
+test_that("empty CONTACTSURVEYS_HOME falls back to R_user_dir when env empty", {
   withr::local_envvar(CONTACTSURVEYS_HOME = "")
   p <- contactsurveys_dir()
   expect_true(nzchar(p))
-  expect_true(file.exists(p))
+  expect_identical(
+    p,
+    tools::R_user_dir("contactsurveys")
+  )
 })
