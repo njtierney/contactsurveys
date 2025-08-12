58a48c2bb24a386624d2982fa773125b0add4dce
njtierney <nicholas.tierney@gmail.com>
2025-08-12 16:32:06 +1000

    exploring issues with downloading/checking

--- inst/dev/check_all_surveys.r
+++ inst/dev/check_all_surveys.r
@@ -1,21 +1,26 @@
 library(socialmixr) # nolint
 library(purrr)
 library(here) # nolint
+library(tictoc) # nolint
 
 ## load list of survey files
 survey_files <- readRDS(here("surveys", "survey_files.rds"))
 
 ## define safe checking function
 safe_as_contact_survey <- safely(\(files) as_contact_survey(load_survey(files)))
-
+tic()
 ## check all surveys
 checks <- map(survey_files, safe_as_contact_survey)
 
 errors <- map(checks, "error")
 no_error <- map_vec(errors, is.null)
 error_messages <- map(errors[!no_error], "message")
-
+toc()
 cat("Errors:\n\n")
 error_messages
 cat("\n\nWarnings:\n\n")
 warnings()
+
+file_names_that_error <- names(error_messages)
+debugonce(load_survey)
+load_survey(survey_files[[file_names_that_error[1]]])
--- inst/dev/dl_all_surveys.r
+++ inst/dev/dl_all_surveys.r
@@ -12,23 +12,19 @@ dir.create(here("surveys"), showWarnings = FALSE)
 # use `possibly` to ensure this continues running
 
 possibly_download_survey <- possibly(.f = download_survey, otherwise = NULL)
-
 tic()
 survey_files <- map(ls$url, function(x) {
-  Sys.sleep(5)
+  Sys.sleep(10)
   survey_downloads <- possibly_download_survey(
     survey = x,
-    dir = "surveys"
+    dir = "surveys",
+    overwrite = TRUE
   )
 })
-
-
 toc()
-# identify issue with download
+
 which_did_not_download <- which(map(survey_files, is.null) |> flatten_lgl())
 
-browseURL(ls$url[which_did_not_download])
-https://zenodo.org/records/3878754
 ## name list elements according to url
 names(survey_files) <- paste0(ls$title, " (", ls$url, ")")
 ## save list of survey files
